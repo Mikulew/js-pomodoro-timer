88657dbe2a91e3fe150ecd1a6bfe68a650f2ac87
Mikulew <mikulew@gmail.com>
2022-01-18 19:22:10 +0100

    Add countDown to timer

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import { initTimer } from './utils/timer.js';
 import { handleTabClick } from './utils/tabs.js';
 
-const startBtn = document.getElementById("start");
 const tabs = document.querySelectorAll('.tab');
 
 initTimer();
@@ -9,5 +8,3 @@ initTimer();
 tabs.forEach((tab, index) => {
   tab.addEventListener('click', () => handleTabClick(tabs, index));
 });
-
-startBtn.addEventListener('click', () => console.log('clicked!'));
--- utils/timer.js
+++ utils/timer.js
@@ -1,14 +1,39 @@
 import { TABS, DEFAULT_TIMER_VALUES } from '../consts/index.js';
 
+const startBtn = document.getElementById("start");
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 
 export const changeTimer = (tab) => {
-  console.log(DEFAULT_TIMER_VALUES[tab]);
   minutes.innerText = formatValue(DEFAULT_TIMER_VALUES[tab].min);
   seconds.innerText = formatValue(DEFAULT_TIMER_VALUES[tab].sec);
 };
 
-export const initTimer = () => changeTimer(TABS.POMODORO);
+export const initTimer = () => {
+  changeTimer(TABS.POMODORO);
+  startBtn.addEventListener('click', () => countDown());
+}
 
 export const formatValue = (number) => number < 10 ? `0${number}` : number;
+
+export const countDown = () => {
+  const timer = setInterval(() => {
+    let tempMinutes = Number.parseInt(minutes.innerText);
+    let tempSeconds = Number.parseInt(seconds.innerText);
+    startBtn.disabled = true;
+    if (tempMinutes === 0 && tempSeconds === 0) {
+      startBtn.disabled = false;
+      minutes.innerText = formatValue(tempMinutes);
+      seconds.innerText = formatValue(tempSeconds);
+      return clearInterval(timer);
+    }
+    if (tempSeconds === 0) {
+      tempSeconds = 59;
+      tempMinutes--;
+    } else {
+      tempSeconds--;
+    }
+    minutes.innerText = formatValue(tempMinutes);
+    seconds.innerText = formatValue(tempSeconds);
+  }, 1000);
+};
