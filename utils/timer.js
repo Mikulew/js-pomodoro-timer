1d264d5a3c2e4cbdfd093dfc28122d5842d7dad9
Mikulew <mikulew@gmail.com>
2022-01-19 16:01:43 +0100

    Fix reset timer's issue

--- utils/timer.js
+++ utils/timer.js
@@ -3,37 +3,44 @@ import { TABS, DEFAULT_TIMER_VALUES } from '../consts/index.js';
 const startBtn = document.getElementById("start");
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
+let timer = null;
+
+const formatValue = (number) => number < 10 ? `0${number}` : number;
 
 export const changeTimer = (tab) => {
   minutes.innerText = formatValue(DEFAULT_TIMER_VALUES[tab].min);
   seconds.innerText = formatValue(DEFAULT_TIMER_VALUES[tab].sec);
+  restartTimer();
 };
 
 export const initTimer = () => {
   changeTimer(TABS.POMODORO);
-  startBtn.addEventListener('click', () => countDown());
+  startBtn.addEventListener('click', () => startTimer());
 }
 
-export const formatValue = (number) => number < 10 ? `0${number}` : number;
+const startTimer = () => timer = setInterval(countDown, 1000);
 
-export const countDown = () => {
-  const timer = setInterval(() => {
-    let tempMinutes = Number.parseInt(minutes.innerText);
-    let tempSeconds = Number.parseInt(seconds.innerText);
-    startBtn.disabled = true;
-    if (tempMinutes === 0 && tempSeconds === 0) {
-      startBtn.disabled = false;
-      minutes.innerText = formatValue(tempMinutes);
-      seconds.innerText = formatValue(tempSeconds);
-      return clearInterval(timer);
-    }
-    if (tempSeconds === 0) {
-      tempSeconds = 59;
-      tempMinutes--;
-    } else {
-      tempSeconds--;
-    }
+const restartTimer = () => {
+  startBtn.disabled = false;
+  return clearInterval(timer);
+};
+
+function countDown() {
+  let tempMinutes = Number.parseInt(minutes.innerText);
+  let tempSeconds = Number.parseInt(seconds.innerText);
+  startBtn.disabled = true;
+  if (tempMinutes === 0 && tempSeconds === 0) {
+    startBtn.disabled = false;
     minutes.innerText = formatValue(tempMinutes);
     seconds.innerText = formatValue(tempSeconds);
-  }, 1000);
-};
+    return clearInterval(timer);
+  }
+  if (tempSeconds === 0) {
+    tempSeconds = 59;
+    tempMinutes--;
+  } else {
+    tempSeconds--;
+  }
+  minutes.innerText = formatValue(tempMinutes);
+  seconds.innerText = formatValue(tempSeconds);
+}
