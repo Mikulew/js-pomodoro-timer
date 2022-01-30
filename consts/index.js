6930ab0ee6906a15a292707ef3fec34d5c218867
Mikulew <mikulew@gmail.com>
2022-01-30 13:18:04 +0100

    Add allowed character valdiation for inputs type number

--- consts/index.js
+++ consts/index.js
@@ -26,3 +26,5 @@ export const DEFAULT_SOUNDTRACK_PATH = {
 };
 
 export const SOUNDTRACK_PLAYING_TIME = 2500;
+
+export const NOT_ALLOWED_CHARACTERS = ['-', 'e', 'E', '+'];
--- utils/settings.js
+++ utils/settings.js
@@ -1,6 +1,15 @@
+import { NOT_ALLOWED_CHARACTERS } from '../consts/index.js';
+
 const settings = document.getElementById('settings');
 const closeButton = document.getElementById('closeButton');
 const saveButton = document.getElementById('save');
+const pomodoroMinutes = document.getElementById('pomodoro-minutes');
+const pomodoroSeconds = document.getElementById('pomodoro-seconds');
+const shortBreakMinutes = document.getElementById('short-break-minutes');
+const shortBreakSeconds = document.getElementById('short-break-seconds');
+const longBreakMinutes = document.getElementById('long-break-minutes');
+const longBreakSeconds = document.getElementById('long-break-seconds');
+const inputs = [pomodoroMinutes, pomodoroSeconds, shortBreakMinutes, shortBreakSeconds, longBreakMinutes, longBreakSeconds];
 
 const toggleSettings = () => {
   if (settings.classList.contains('hide')) {
@@ -10,8 +19,19 @@ const toggleSettings = () => {
   }
 };
 
+const validateAllowedCharacters = e => {
+  if (NOT_ALLOWED_CHARACTERS.includes(e.key)) {
+    return e.preventDefault();
+  }
+  return null;
+};
+
 closeButton.addEventListener('click', () => settings.classList.add('hide'));
 
 saveButton.addEventListener('click', () => console.log('save – to do'));
+
+inputs.forEach(input => {
+  input.addEventListener('keypress', validateAllowedCharacters);
+});
 
 export default toggleSettings;
